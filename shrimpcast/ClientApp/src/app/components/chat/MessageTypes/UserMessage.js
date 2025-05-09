1932c14bc17273405a2ad137d22830f8663848ea
shrimpcast <167498236+shrimpcast@users.noreply.github.com>
2025-05-09 14:39:52 -0300

    Revert ea0196fee45825be1dcab627f4e13b9a9db34112

--- shrimpcast/ClientApp/src/app/components/chat/MessageTypes/UserMessage.js
+++ shrimpcast/ClientApp/src/app/components/chat/MessageTypes/UserMessage.js
@@ -75,6 +75,8 @@ const WrapperTextBoxSx = {
     },
   },
   SourceLinkSx = {
+    ml: "2.5px",
+    mr: "2.5px",
     fontWeight: "bold",
     fontSize: "15px",
     color: "secondary.main",
@@ -100,7 +102,7 @@ const UserMessage = React.memo((props) => {
     escapedName = LocalStorageManager.getName().replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
     // Use lookahead assertion to ensure we're matching the full name
     nameRegex = `@${escapedName}(?=[\\s.]|$)`,
-    sourcesRegex = `(?:^|)(?:${sources})(?=\\s|$)`,
+    sourcesRegex = `(?:^|\\s)(?:${sources})(?=\\s|$)`,
     urlRegex = "https?://\\S+",
     regex = new RegExp(
       `(${nameRegex}|${urlRegex}${emotesRegex ? "|" + emotesRegex : ""}${sources ? "|" + sourcesRegex : ""})`,
