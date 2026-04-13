676f02ca645c794aaafae44e2aa438b2a67a9c3d
shrimpcast <167498236+shrimpcast@users.noreply.github.com>
2026-04-13 19:47:15 -0300

    Bump version

--- shrimpcast/Entities/Constants.cs
+++ shrimpcast/Entities/Constants.cs
@@ -4,7 +4,7 @@ namespace shrimpcast.Entities
 {
     public class Constants
     {
-        public const string BACKEND_VERSION = "2.0.9";
+        public const string BACKEND_VERSION = "2.1.0";
 
         public const string PING_COMMAND = "!ping";
 
