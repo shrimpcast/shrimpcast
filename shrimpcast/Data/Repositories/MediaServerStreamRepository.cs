7c4d79b79c4a95a89554d4166db2cf96cff43e91
shrimpcast <167498236+shrimpcast@users.noreply.github.com>
2026-06-19 07:10:06 -0300

    Reduce min `HLS` segment length to 1 second

--- shrimpcast/Data/Repositories/MediaServerStreamRepository.cs
+++ shrimpcast/Data/Repositories/MediaServerStreamRepository.cs
@@ -53,7 +53,7 @@ namespace shrimpcast.Data.Repositories.Interfaces
 
         private async Task Validate(MediaServerStream stream)
         {
-            if (stream.SnapshotInterval < 15 || stream.SegmentLength < 2 || stream.ListSize < 6)
+            if (stream.SnapshotInterval < 15 || stream.SegmentLength < 1 || stream.ListSize < 6)
             {
                 throw new InvalidDataException();
             }
