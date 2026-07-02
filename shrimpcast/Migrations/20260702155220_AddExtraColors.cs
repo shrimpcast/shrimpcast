b11ac2a486c90bf1b2784802196d775e3e307541
shrimpcast <167498236+shrimpcast@users.noreply.github.com>
2026-07-02 13:25:57 -0300

    Add extra user chat colors

---
+++ shrimpcast/Migrations/20260702155220_AddExtraColors.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using shrimpcast.Entities.DB;
+
+#nullable disable
+
+namespace shrimpcast.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExtraColors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("NameColour", [nameof(NameColour.ColourHex)], new object[,]
+            {
+               { "#795548"},
+               { "#9e9e9e"},
+               { "#607d8b"},
+            });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+               table: "NameColour",
+               keyColumn: nameof(NameColour.ColourHex),
+               keyValues:
+               [
+                 "#795548",
+                 "#9e9e9e",
+                 "#607d8b",
+               ]);
+        }
+    }
+}
